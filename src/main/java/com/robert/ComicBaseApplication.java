0a26b0b114d156dff2a12741ecb86f9e06609553
Robert Mueller <robert@localhost.localdomain>
2017-01-21 20:11:43 +0100

    Initial commit.

---
+++ src/main/java/com/robert/ComicBaseApplication.java
@@ -0,0 +1,12 @@
+package com.robert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicBaseApplication.class, args);
+	}
+}
